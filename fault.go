e128633a6aa2162693498f71ba0da38739420bb8
isaac <48937849+readpe@users.noreply.github.com>
2021-11-24 11:01:52 -0800

    cleaned up fault.go

--- fault.go
+++ fault.go
@@ -4,22 +4,90 @@
 
 package goolx
 
-// FaultConn represents a fault connection for use with the DoFault procedure.
-// The index and code are specified in ASPEN Oneliner documentation.
-type faultConn struct {
-	idx    int
-	code   int
-	seCode float64 // stepped event connection code
+// FltConn represents a fault connection for use with the DoFault procedure.
+// The codes are applied to FaultConfig and SteppedEventConfig as specified in ASPEN Oneliner documentation.
+type FltConn int
+
+// applyToFaultConfig applies the appropriate fault connection code to the provided FaultConfig.
+func (fc FltConn) applyToFaultConfig(cfg *FaultConfig) {
+	switch fc {
+	case ABC:
+		cfg.fltConn[0] = 1
+	case BCG:
+		cfg.fltConn[1] = 1
+	case CAG:
+		cfg.fltConn[1] = 2
+	case ABG:
+		cfg.fltConn[1] = 3
+	case AG:
+		cfg.fltConn[2] = 1
+	case BG:
+		cfg.fltConn[2] = 2
+	case CG:
+		cfg.fltConn[2] = 3
+	case BC:
+		cfg.fltConn[3] = 1
+	case CA:
+		cfg.fltConn[3] = 2
+	case AB:
+		cfg.fltConn[3] = 3
+	}
 }
 
-// Fault connections.
-// TODO(readpe): Populate remaining connection codes.
-var (
-	ABC = faultConn{idx: 0, code: 1, seCode: 1}
-	ABG = faultConn{idx: 1, code: 1, seCode: 4}
-	AG  = faultConn{idx: 2, code: 1, seCode: 5}
-	AB  = faultConn{idx: 3, code: 1, seCode: 8}
+// applyToSteppedEventConfig applies the appropriate fault connection code to the provided SteppedEventConfig.
+func (fc FltConn) applyToSteppedEventConfig(cfg *SteppedEventConfig) {
+	switch fc {
+	case ABC:
+		cfg.fltOpt[0] = 1
+	case BCG:
+		cfg.fltOpt[0] = 2
+	case CAG:
+		cfg.fltOpt[0] = 3
+	case ABG:
+		cfg.fltOpt[0] = 4
+	case AG:
+		cfg.fltOpt[0] = 5
+	case BG:
+		cfg.fltOpt[0] = 6
+	case CG:
+		cfg.fltOpt[0] = 7
+	case BC:
+		cfg.fltOpt[0] = 8
+	case CA:
+		cfg.fltOpt[0] = 9
+	case AB:
+		cfg.fltOpt[0] = 10
+	}
+}
+
+// Fault connection codes.
+const (
+	// Three phase fault.
+	ABC FltConn = iota
+	// Phase-Phase-Ground faults.
+	BCG
+	CAG
+	ABG
+
+	// Phase-Ground faults.
+	AG
+	BG
+	CG
+
+	// Phase-Phase faults.
+	BC
+	CA
+	AB
 )
+
+// // Fault connections.
+// // TODO(readpe): Populate remaining connection codes.
+// var (
+// 	ABC = FltConn{idx: 0, code: 1, seCode: 1}
+// 	ABG = FltConn{idx: 1, code: 1, seCode: 4}
+// 	AG  = FltConn{idx: 2, code: 1, seCode: 5}
+// 	AB  = FltConn{idx: 3, code: 1, seCode: 8}
+// )
 
 // OutageOption represents a the method outages are applied with use in the DoFault procedure.
 type OutageOption int
@@ -44,21 +112,14 @@ type FaultConfig struct {
 	clearPrev  bool
 }
 
-// Apply will apply the provided FaultOption functions to the existing config.
-func (cfg *FaultConfig) Apply(options ...FaultOption) {
-	for _, f := range options {
-		f(cfg)
-	}
-}
-
 // NewFaultConfig returns a pointer to a new instance of FaultConfig for use with the Oneliner
 // DoFault procedure. Provide FaultOption functions to modify the underlying parameters.
 func NewFaultConfig(options ...FaultOption) *FaultConfig {
-	fc := &FaultConfig{
+	cfg := &FaultConfig{
 		outageList: make([]int, 1),
 	}
-	fc.Apply(options...)
-	return fc
+	FaultOptions(options...)(cfg)
+	return cfg
 }
 
 // New3LGFaultConfig is a helper function to return a new 3LG FaultConfig instance. See NewFaultConfig for
@@ -76,30 +137,37 @@ func New1LGFaultConfig() *FaultConfig {
 // FaultOption represents configuration modification functions to apply to the FaultConfig data.
 type FaultOption func(*FaultConfig)
 
+//FaultOptions consolidates a list of FaultOptions to a single FaultOption.
+func FaultOptions(options ...FaultOption) FaultOption {
+	return func(cfg *FaultConfig) {
+		for _, opt := range options {
+			opt(cfg)
+		}
+	}
+}
+
 // FaultRX sets the fault impedance in Ohms.
 func FaultRX(r, x float64) FaultOption {
-	return func(fc *FaultConfig) {
-		fc.fltR = r
-		fc.fltX = x
+	return func(cfg *FaultConfig) {
+		cfg.fltR = r
+		cfg.fltX = x
 	}
 }
 
 // FaultClearPrev sets the clear previous flag. True will clear the
 // previous fault results.
 func FaultClearPrev(e bool) FaultOption {
-	return func(fc *FaultConfig) {
-		fc.clearPrev = e
+	return func(cfg *FaultConfig) {
+		cfg.clearPrev = e
 	}
 }
 
 // FaultConn applies the provided fault connections. Overrides the previous fault connections.
-func FaultConn(conn ...faultConn) FaultOption {
-	var fltConn [4]int
-	for _, c := range conn {
-		fltConn[c.idx] = c.code
-	}
-	return func(fc *FaultConfig) {
-		fc.fltConn = fltConn
+func FaultConn(conn ...FltConn) FaultOption {
+	return func(cfg *FaultConfig) {
+		for _, c := range conn {
+			c.applyToFaultConfig(cfg)
+		}
 	}
 }
 
@@ -119,105 +187,105 @@ func withOutage(f FaultOption, outageList []int, otgOpt OutageOption) FaultOptio
 		outageOptions[3] = 1
 	}
 
-	return func(fc *FaultConfig) {
-		f(fc) // Call wrapped FaultOption function first.
-		fc.outageList = outageList
-		fc.outageOpt = outageOptions
+	return func(cfg *FaultConfig) {
+		f(cfg) // Call wrapped FaultOption function first.
+		cfg.outageList = outageList
+		cfg.outageOpt = outageOptions
 	}
 }
 
 // FaultCloseIn applies a close-in fault.
 func FaultCloseIn() FaultOption {
-	return func(fc *FaultConfig) {
-		fc.fltOpt[0] = 1
+	return func(cfg *FaultConfig) {
+		cfg.fltOpt[0] = 1
 	}
 }
 
 // FaultCloseInOutage applies a close-in fault with outages on the system. An outage list and options are required.
 func FaultCloseInOutage(outageList []int, otgOpt OutageOption) FaultOption {
-	return withOutage(func(fc *FaultConfig) {
-		fc.fltOpt[1] = 1
+	return withOutage(func(cfg *FaultConfig) {
+		cfg.fltOpt[1] = 1
 	}, outageList, otgOpt)
 }
 
 // FaultCloseInEndOpen applies a close-in fault with the end open as determined by the Oneliner algorithm for determining the
 // end of the line.
 func FaultCloseInEndOpen() FaultOption {
-	return func(fc *FaultConfig) {
-		fc.fltOpt[2] = 1
+	return func(cfg *FaultConfig) {
+		cfg.fltOpt[2] = 1
 	}
 }
 
 // FaultCloseInEndOpenOutage applies a close-in fault with outages and the end open as determined by the Oneliner algorithm for determining the
 // end of the line. An outage list and options are required.
 func FaultCloseInEndOpenOutage(outageList []int, otgOpt OutageOption) FaultOption {
-	return withOutage(func(fc *FaultConfig) {
-		fc.fltOpt[3] = 1
+	return withOutage(func(cfg *FaultConfig) {
+		cfg.fltOpt[3] = 1
 	}, outageList, otgOpt)
 }
 
 // FaultRemoteBus applies a remote bus fault.
 func FaultRemoteBus() FaultOption {
-	return func(fc *FaultConfig) {
-		fc.fltOpt[4] = 1
+	return func(cfg *FaultConfig) {
+		cfg.fltOpt[4] = 1
 	}
 }
 
 // FaultRemoteBusOutage applies a remote bus fault with outages. An outage list and options are required.
 func FaultRemoteBusOutage(outageList []int, otgOpt OutageOption) FaultOption {
-	return withOutage(func(fc *FaultConfig) {
-		fc.fltOpt[5] = 1
+	return withOutage(func(cfg *FaultConfig) {
+		cfg.fltOpt[5] = 1
 	}, outageList, otgOpt)
 }
 
 // FaultLineEnd applies a line end fault, as determined by Oneliners algorithm for determining line end.
 func FaultLineEnd() FaultOption {
-	return func(fc *FaultConfig) {
-		fc.fltOpt[6] = 1
+	return func(cfg *FaultConfig) {
+		cfg.fltOpt[6] = 1
 	}
 }
 
 // FaultLineEndOutage applies a line end fault with outages. Line end is determined by Oneliners algorithm for determining line end. An outage list and options are required.
 func FaultLineEndOutage(outageList []int, otgOpt OutageOption) FaultOption {
-	return withOutage(func(fc *FaultConfig) {
-		fc.fltOpt[7] = 1
+	return withOutage(func(cfg *FaultConfig) {
+		cfg.fltOpt[7] = 1
 	}, outageList, otgOpt)
 }
 
 // FaultIntermediate applies an intermediate fault at the provided percentage.
 func FaultIntermediate(percent float64) FaultOption {
-	return func(fc *FaultConfig) {
-		fc.fltOpt[8] = percent
+	return func(cfg *FaultConfig) {
+		cfg.fltOpt[8] = percent
 	}
 }
 
 // FaultIntermediate applies an intermediate fault with outage at the provided percentage. An outage list and options are required.
 func FaultIntermediateOutage(percent float64, outageList []int, otgOpt OutageOption) FaultOption {
-	return withOutage(func(fc *FaultConfig) {
-		fc.fltOpt[9] = percent
+	return withOutage(func(cfg *FaultConfig) {
+		cfg.fltOpt[9] = percent
 	}, outageList, otgOpt)
 }
 
 // FaultIntermediate applies an intermediate fault with at the provided percentage with the end open.
 func FaultIntermediateEndOpen(percent float64) FaultOption {
-	return func(fc *FaultConfig) {
-		fc.fltOpt[10] = percent
+	return func(cfg *FaultConfig) {
+		cfg.fltOpt[10] = percent
 	}
 }
 
 // FaultIntermediateEndOpenOutage applies an intermediate fault with at the provided percentage with the end open and outages. An outage list and options are required.
 func FaultIntermediateEndOpenOutage(percent float64, outageList []int, otgOpt OutageOption) FaultOption {
-	return withOutage(func(fc *FaultConfig) {
-		fc.fltOpt[11] = percent
+	return withOutage(func(cfg *FaultConfig) {
+		cfg.fltOpt[11] = percent
 	}, outageList, otgOpt)
 }
 
 // FaultIntermediateAuto applies an auto sequencing intermediate fault between from and to at the specified step.
 func FaultIntermediateAuto(step, from, to float64) FaultOption {
-	return func(fc *FaultConfig) {
-		fc.fltOpt[9] = step
-		fc.fltOpt[12] = from
-		fc.fltOpt[13] = to
+	return func(cfg *FaultConfig) {
+		cfg.fltOpt[9] = step
+		cfg.fltOpt[12] = from
+		cfg.fltOpt[13] = to
 	}
 }
 
@@ -250,9 +318,9 @@ func SteppedEventOptions(options ...SteppedEventOption) SteppedEventOption {
 }
 
 // SteppedEventConn sets the fault connection code.
-func SteppedEventConn(conn faultConn) SteppedEventOption {
+func SteppedEventConn(conn FltConn) SteppedEventOption {
 	return func(cfg *SteppedEventConfig) {
-		cfg.fltOpt[0] = conn.seCode
+		conn.applyToSteppedEventConfig(cfg)
 	}
 }
 
